46e69ca3df33dddb82de8f3a5e5c8307d617ba49
bo liu <liubo0331@gmail.com>
2014-08-07 21:06:17 -0700

    Finally get it work.

--- client/collections/SongQueue.js
+++ client/collections/SongQueue.js
@@ -2,6 +2,33 @@
 var SongQueue = Songs.extend({
 
   initialize: function(){
-  }
+  	this.on('add', this.enqueue, this);
+  	this.on('dequeue', this.dequeue, this);
+  },
 
+  enqueue: function(song){
+  	if(this.length === 1){
+  		this.playFirst();
+  	}
+  },
+
+  dequeue: function(song){
+  	if(this.at(0) === song){
+  		this.playNext();
+  	}
+  	this.remove(song);
+  },
+
+  playFirst: function(){
+  	this.at(0).play();
+  },
+
+  playNext: function(){
+  	this.shift();
+  	if(this.length > 0){
+	 	this.playFirst();
+	}else{
+		this.trigger('stop');
+	}
+  }
 });
